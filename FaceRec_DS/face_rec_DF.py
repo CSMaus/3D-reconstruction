87fa1e21fe0a6ff5497ac8ebca763d73d9546a43
Kseniia Barashok <cs-maus-1@mail.ru>
2024-02-05 23:21:55 +0900

    TODO

--- FaceRec_DS/face_rec_DF.py
+++ FaceRec_DS/face_rec_DF.py
@@ -1,5 +1,6 @@
 from deepface import DeepFace
-models = ['VGG-Face', 'Facenet', 'OpenFace', 'DeepFace', 'DeepID', 'Dlib']
-DeepFace.stream(db_path="captured_faces", model_name=models[1])
+import cv2
 
-verification = DeepFace.verify(img1_path="img1.jpg", img2_path = "img2.jpg")
+models = ['VGG-Face', 'Facenet', 'OpenFace', 'DeepFace', 'DeepID', 'Dlib']
+# so, example iof how to use with video - realtime.py impementation. insted of path use numpy array
+DeepFace.stream(db_path="captured_faces/", model_name=models[3])
